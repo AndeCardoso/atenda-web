073e7ffcce4731b0387b8e8e909b159e1e919d03
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-04-10 22:11:41 -0300

    feat: adding some components of mui for test and experimentation in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import { Button, InputLabel, OutlinedInput } from '@mui/material';
+import Container from '@mui/material/Container';
+import { useAppThemeContext } from 'shared/context/ThemeContext';
+
 export const Home = () => {
+  const [name, setName] = useState('teset');
+
+  const { toggleTheme } = useAppThemeContext();
+
   return (
-    <>
-      <h1>Atenda</h1>
-    </>
-  )
-}
+    <Container>
+      <InputLabel htmlFor='nome'>Nome</InputLabel>
+      <OutlinedInput placeholder='Ex.: Maria' name='nome' />
+
+      <Button variant='contained' onClick={toggleTheme}>Mudar Tema</Button>
+    </Container>
+  );
+};
