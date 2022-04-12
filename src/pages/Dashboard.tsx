4f41e320cd0bd5017f4afa816d5a11a623b51ce1
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-04-11 23:46:23 -0300

    style: adding drawer menu with toggle button and theme change with check button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+import { Box, IconButton, Switch, useTheme } from '@mui/material';
+import { Menu, LightbulbOutlined } from '@mui/icons-material';
 
-export const Dashboard = () => {  
+import { SideBar } from 'shared/components';
+import { useAppThemeContext } from 'shared/context/ThemeContext';
+
+export const Dashboard = () => {
+  const theme = useTheme();
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [margin, setMargin] = useState<string>(theme.spacing(1));
+
+  const toggleSidebar = () => {
+    setOpen(open ? false : true);
+    setMargin(margin === theme.spacing(1) ? theme.spacing(28) : theme.spacing(1));
+  };
+
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <>
+      <SideBar open={open}/>
+      <Box
+        height="100vh"
+        marginLeft={margin}
+        display="flex"
+        alignItems="flex-start"
+        justifyContent="space-between"
+      >
+        <IconButton onClick={toggleSidebar}>
+          <Menu />
+        </IconButton>
+        <Box display="flex" alignItems="center">
+          <LightbulbOutlined />
+          <Switch defaultChecked onChange={toggleTheme} />
+        </Box>
+      </Box>
     </>
   );
 };
