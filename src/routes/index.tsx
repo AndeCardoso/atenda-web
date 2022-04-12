3079db6a48c470114dff0987ad2c9d59a53cef2f
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-04-11 23:49:39 -0300

    feat: adding dashboard route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes as Switch, Route, Navigate } from 'react-router-dom';
-import { Home } from 'pages/Home';
+import { Home, Dashboard } from 'pages';
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Switch>
     </BrowserRouter>
