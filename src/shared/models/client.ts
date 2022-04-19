8c3e76a7b7077e3fd7fc45db70c6733047683b92
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-04-18 22:58:23 -0300

    feat: add models types of client and technician

---
+++ src/shared/models/client.ts
@@ -0,0 +1,16 @@
+export interface IClientForm {
+  cpf?: string;
+  name: string;
+  phone: string;
+  email?: string;
+  address: IAddress;
+}
+
+interface IAddress {
+  street?: string;
+  number?: number;
+  city?: string;
+  district?: string;
+  state?: string;
+  cep?: string;
+}
