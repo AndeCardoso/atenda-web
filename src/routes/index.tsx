0e9c6c2ef728da2b9dfae51f9aefc630d865c9e2
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-04-18 22:52:46 -0300

    feat: add calendar component and route to same

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes as Switch, Route, Navigate } from 'react-router-dom';
 import { Home, Dashboard } from 'pages';
 import { FormClient } from 'shared/components';
+import { FormOs } from 'shared/components/forms/form-os/FormOs';
+import { Calendar } from 'shared/components/calendar/Calendar';
+import { FormTechnician } from 'shared/components/forms/form-technician/FormTechnician';
 
 export const Routes = () => {
   return (
@@ -8,9 +11,10 @@ export const Routes = () => {
       <Switch>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard/" element={<Dashboard />} >
-          <Route path='cadastro-os' element={<h1>Cadastro OS</h1>} />
+          <Route path='cadastro-os' element={<FormOs />} />
           <Route path='cadastro-cliente' element={<FormClient />} />
-          <Route path='atendimento' element={<h1>Atendimento</h1>} />
+          <Route path='atendimento' element={<Calendar />} />
+          <Route path='cadastro-tecnico' element={<FormTechnician />} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
       </Switch>
