ec6f32bebc24d9d78799e81759e317d5e55dc83f
AndeCardoso <andersoncardoso.dev@gmail.com>
2022-06-11 11:52:48 -0300

    refact: changing password input to the component appinputpassword

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
 export * from './alert/AppAlert';
 export * from './input/AppInput';
+export * from './section-title/SectionTitle';
 export * from './toggle-theme/ToggleTheme';
--- src/shared/input/AppInput.tsx
+++ src/shared/input/AppInput.tsx
@@ -1,38 +1,116 @@
-import { TextField } from '@mui/material';
-import { HTMLInputTypeAttribute } from 'react';
-import { Controller } from 'react-hook-form';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+import {
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField
+} from '@mui/material';
+import { HTMLInputTypeAttribute, useState } from 'react';
+import { Control, Controller, FieldValues } from 'react-hook-form';
 
 interface IInput {
-  control: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<FieldValues | any>;
   label: string;
-  id: string;
-  type: HTMLInputTypeAttribute;
+  name: string;
+  type?: HTMLInputTypeAttribute;
   placeholder?: string;
   helper?: string;
+  pattern?: RegExp;
   required: boolean;
 }
 
-export const AppInput = ({ label, id, type, required, control, placeholder, helper }: IInput) => {
+export const AppInput = ({ label, name, type, required, control, placeholder, helper, pattern }: IInput) => {
   return (
     <Controller
       control={control}
-      rules={{ required }}
-      name={id}
+      rules={{ required, pattern, }}
+      name={name}
       render={({
-        field: { onChange, value }, fieldState: { error }
+        field: { onChange, value },
+        fieldState: { error }
       }) => (
         <TextField
           variant="outlined"
           size="small"
           fullWidth
           label={label}
-          id={id}
           error={!!error}
-          helperText={error ? helper : null}
+          helperText={error ? helper : undefined}
           type={type}
           onChange={onChange}
           value={value}
-          placeholder={placeholder} />
-      )}/>
+          placeholder={placeholder}
+        />
+      )}
+    />
+  );
+};
+
+interface IState {
+  password: string;
+  showPassword: boolean;
+}
+
+export const AppInputPassword = ({ label, name, required, control, helper, pattern }: IInput) => {
+  const [values, setValues] = useState<IState>({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Controller
+      control={control}
+      rules={{ required, pattern, }}
+      name={name}
+      render={({
+        field: { onChange, value },
+        fieldState: { error }
+      }) => (
+        <FormControl
+          variant="outlined"
+          size="small"
+          fullWidth
+        >
+          <InputLabel htmlFor="password">Senha</InputLabel>
+          <OutlinedInput
+            id={name}
+            label={label}
+            type={values.showPassword ? 'text' : 'password'}
+            value={value}
+            onChange={onChange}
+            error={!!error}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+          {
+            error ? <FormHelperText error id={label}>{helper}</FormHelperText> : <></>
+          }
+        </FormControl>
+      )}
+    />
   );
 };
